7d2a528203b59ee853f31e5ab762b7d93bd727b3
Ashminita <ashminitab@mindfiresolutions.com>
2026-02-05 16:01:30 +0530

    feat: add dockerfile nd compose docker and nginix

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
+// ONLY FOR TESTING THE LOCAL HOST BY GETTING THE RESPONSE
+app.get('/', (req, res) => {
+    res.send('Hello world!!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
